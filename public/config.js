71ee01618ccea3af45ede989899e797f99d3592c
YashikaSolanki011 <solankiyaashika@gmail.com>
2026-09-23 09:55:50 +0530

    Update Supabase URL in config.js

--- public/config.js
+++ public/config.js
@@ -1,1 +1,1 @@
-window.MEDIKIOSK_CONFIG={supabaseUrl:"https://YOUR_PROJECT_REF.supabase.co",supabaseAnonKey:"sb_publishable_DPQMMJgwRwJCu_6this28Q_ykQ28w_P"};
+window.MEDIKIOSK_CONFIG={supabaseUrl:"https://oqmdzkimrpxqkvsrxeuc.supabase.co",supabaseAnonKey:"sb_publishable_DPQMMJgwRwJCu_6this28Q_ykQ28w_P"};
